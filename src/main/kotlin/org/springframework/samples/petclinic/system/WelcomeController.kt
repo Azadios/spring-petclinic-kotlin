dfc64c9851707e5e87311aa08fdc044e10385c3f
Azad Abdulov <azad.abdulov@jetbrains.com>
2026-06-26 12:00:28 +0000

    Add WelcomeController comment

--- src/main/kotlin/org/springframework/samples/petclinic/system/WelcomeController.kt
+++ src/main/kotlin/org/springframework/samples/petclinic/system/WelcomeController.kt
@@ -19,6 +19,8 @@ import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
 
 /**
+ * Handles rendering the Petclinic welcome page.
+ *
  * @author Antoine Rey
  */
 @Controller
